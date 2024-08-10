2d6e9322e901d4d9bffc41d31395e175320b804b
madmaxieee <76544194+madmaxieee@users.noreply.github.com>
2024-08-10 14:12:34 +0800

    copy function over from my dotfiles

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # vipe.fish
 
-A vipe implementation in fish shell.
+A vipe implementation in fish shell ported from a [shell implementation](https://github.com/juliangruber/vipe/blob/master/vipe.sh).
---
+++ functions/vipe.fish
@@ -0,0 +1,23 @@
+# ported from https://github.com/juliangruber/vipe/blob/master/vipe.sh
+
+function vipe -d "edit pipe with \$EDITOR"
+    # accepts no arguments
+    if test (count $argv) -gt 0
+        echo "usage: vipe"
+        return 1
+    end
+
+    set tmp_file /tmp/vipe.(random).txt
+    touch $tmp_file
+
+    if not isatty stdin
+        cat >$tmp_file
+    end
+
+    # spawn editor with stdio connected
+    $EDITOR $tmp_file </dev/tty >/dev/tty || return $status
+
+    cat $tmp_file
+
+    rm $tmp_file
+end
